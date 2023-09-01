5f1a0fa60c3b7e1e869235316b82882962d9eead
composercordero <composercordero@gmail.com>
2023-09-01 17:55:22 -0500

    button change view and dark mode

--- dist/bundle.js
+++ dist/bundle.js
@@ -222,29 +222,36 @@ const nav = document.getElementsByClassName('navBtn');
 for (let btn of nav) {
     btn.addEventListener('click', (e) => {
         let target = e.currentTarget;
-        switch (target.id) {
-            case 'homeBtn':
-                console.log('home');
-                break;
-            case 'aboutBtn':
-                console.log('about');
-                break;
-            case 'shopBtn':
-                console.log('shop');
-                break;
-            case 'contactBtn':
-                console.log('contact');
-                break;
-            case 'modeBtn':
-                if (document.body.dataset.bsTheme === '') {
-                    document.body.dataset.bsTheme = 'dark';
-                }
-                else {
-                    document.body.dataset.bsTheme = '';
-                    break;
-                }
+        if (target.id === 'modeBtn') {
+            if (document.body.dataset.bsTheme === '') {
+                document.body.dataset.bsTheme = 'dark';
+                const modeBtn = document.getElementById('modeBtn');
+                modeBtn.innerHTML = '<i class="bi bi-brightness-high-fill"></i> <p>Light Mode</p>';
+            }
+            else {
+                document.body.dataset.bsTheme = '';
+                const modeBtn = document.getElementById('modeBtn');
+                modeBtn.innerHTML = '<i class="bi bi-moon-stars-fill"></i> <p>Dark Mode</p>';
+            }
+        }
+        else {
+            changeView(target.id.slice(0, -3));
         }
     });
+}
+function changeView(section) {
+    // Turn off the element(s) that are visible
+    const toTurnOff = document.getElementsByClassName('is-visible');
+    for (let element of toTurnOff) {
+        console.log('Turning off', element);
+        element.classList.replace('is-visible', 'is-invisible');
+        // let navLink = document.getElementsByName(element.id)[0];
+        // navLink.classList.remove('active');
+    }
+    // Turn on the element based on the link that was clicked
+    const toTurnOn = document.getElementById(section);
+    toTurnOn.classList.replace('is-invisible', 'is-visible');
+    // section.target.classList.add('active');
 }
 class User {
     constructor(_name, _age, _cart = [], _id = (0,uuid__WEBPACK_IMPORTED_MODULE_0__["default"])()) {
--- scratch.js
+++ scratch.js
@@ -1,30 +1,29 @@
-// const nav = document.getElementsByClassName('navBtn')
-// for(let btn of nav){
-//     btn!.addEventListener('click',(e:Event) => {
-//         let target = e.currentTarget as HTMLElement
-//         if (target.id === 'mode'){
-//             if(document.body.dataset.bsTheme === ''){
-//                 document.body.dataset.bsTheme = 'dark'
-//             }else{
-//                 document.body.dataset.bsTheme = ''
-//         }}else{
-//         changeView(target.id.slice(0, -3)))
-//         }
-//     })
-// }
 
-// function changeView(section){
-//     // Turn off the element(s) that are visible
-//     const toTurnOff = document.getElementsByClassName('is-visible');
-//     for (let element of toTurnOff){
-//         console.log('Turning off', element);
-//         element.classList.replace('is-visible', 'is-invisible');
-//         let navLink = document.getElementsByName(element.id)[0];
-//         navLink.classList.remove('active');
-//     }
 
-//     // Turn on the element based on the link that was clicked
-//     const toTurnOn = document.getElementById(section);
-//     toTurnOn!.classList.replace('is-invisible', 'is-visible');
-//     event.target.classList.add('active');
-// }
+
+for(let btn of nav){
+    btn!.addEventListener('click',(e:Event) => {
+        let target = e.currentTarget as HTMLElement
+        switch(target.id){
+            case 'homeBtn':
+                console.log('home')
+                break;
+            case 'aboutBtn':
+                console.log('about')
+                break;
+            case 'shopBtn':
+                console.log('shop')
+                break;
+            case 'contactBtn':
+                console.log('contact')
+                break;
+            case 'modeBtn':
+                if(document.body.dataset.bsTheme === ''){
+                    document.body.dataset.bsTheme = 'dark'
+                }else{
+                    document.body.dataset.bsTheme = ''
+                break;
+        }
+    }
+    })
+}
